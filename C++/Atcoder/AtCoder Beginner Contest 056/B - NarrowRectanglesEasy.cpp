3c79104eafe4fe0c2146e0605e05c7129d027535
AKASH-ALAM <mesbahulalamakash2014@gmail.com>
2020-09-22 21:46:42 +0600

    AC

--- C++/Atcoder/AtCoder Beginner Contest 056/B - NarrowRectanglesEasy.cpp
+++ C++/Atcoder/AtCoder Beginner Contest 056/B - NarrowRectanglesEasy.cpp
@@ -17,30 +17,18 @@
 #define rall(x) 	(x).rbegin(), (x).rend()
 #define sz(x) 		(int)x.size()
 #define debug(x) 	cerr << #x << " = " << (x) <<endl
-#define Fast_io    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define Fast_io    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 
 const ld PI = acos((ld)-1);
 const int MOD = 1e9+7;
 const ll INF = 1e18; 
 using namespace std;
-int cnt = 0;
-int sum(int i, int n, int *a){
-    if(i < n){
-        cnt += a[i];
-        sum(i + 1, n, a);
-    }
-    return cnt;
-}
+
 int main(){
     Fast_io;
-    int t;  cin >> t;
-    for(int i = 1; i <= t; i++){
-        int n;  cin >> n;
-        int arr[n];
-        for(int i = 0; i < n; i++) cin >> arr[i];
-        int result = sum(0, n, arr);
-        cout << "Case " << i << ": " << result << endl;
-        cnt = 0;
-    }
+    int w, a, b;    cin >> w >> a >> b;
+    if((a + w) < b) cout << b - (a+w) << endl;
+    else if ((b+w) < a) cout << a  - (b + w)<< endl;
+    else if(a+w >= b && (b+w) >= a) cout << 0 <<  endl;
     return 0;
 }
